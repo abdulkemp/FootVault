109e8c0fc5f5729ef767725188ac264f612e9dc2
Abdul Azeez Kemp <aakemp786@gmail.com>
2023-07-25 09:37:39 +0200

    update

---
+++ src/components/SpinnerHome.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <div class="top">
+      <div class="block"></div>
+    </div>
+    <div class="prod container">
+      <div class="row">
+        <div class="col-md-4 mt-5">
+          <div class="card">
+            <div class="ima">
+              <img
+                :src="item.image"
+                class="card-img-top"
+                alt="..."
+                style="height: 20rem; max-width: 30rem"
+              />
+            </div>
+            <div class="card-body">
+              <h5 class="card-title"></h5>
+              <button class="bt"></button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped></style>
---
+++ src/views/SpinnerView.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <SpinnerHome />
+  </div>
+</template>
+
+<script>
+import SpinnerHome from "../components/SpinnerHome.vue";
+export default {
+  components: {
+    SpinnerHome,
+  },
+};
+</script>
+
+<style scoped></style>
